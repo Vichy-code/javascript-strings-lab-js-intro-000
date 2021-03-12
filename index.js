78e9b39b12f86a694efb0bf4ba1576df9da872e0
Jorge Antonio Vichi Lozada <lozadavichi@gmail.com>
2021-03-12 16:53:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
+  "Hello," + specialGuest + "!"
+var greetSpecialGuest = "Hello," + specialGuest + "!";
 
-var greetSpecialGuest = "" + specialGuest + "!";
-
-var topic = "space";
-
-var conversation = `${topic}`;
+var topic = 'space';
+`Let\'s talk about, ${topic}`
+var conversation = `Let\'s talk about, ${topic}`;
